12e7b804b15152cce49d5e6cd0bf483fa99a3656
Messikommer <nico.messikommer@gmail.com>
2017-06-09 20:33:46 +0200

    fixConv/ConvParallel

--- neural_net/net.py
+++ neural_net/net.py
@@ -40,6 +40,36 @@ def conv_net(input_image, eps_regularization):
                                                activation_fn = tf.nn.relu)
     return output
     
+ def conv_net_parallel(input_image, eps_regularization): 
+
+    conv1 = tf.contrib.layers.conv2d(inputs = input_image, 
+                                    num_outputs = 32, 
+                                    kernel_size = [7,7], 
+                                    padding = "SAME", 
+                                    activation_fn = None,
+                                    weights_regularizer=tf.contrib.layers.l1_regularizer(eps_regularization)
+                                    )
+    pool1 = tf.contrib.layers.max_pool2d(inputs = conv1, kernel_size = [2,2], stride = [1,1])
+    conv2 = tf.contrib.layers.conv2d(inputs = input_image, 
+                                    num_outputs = 32, 
+                                    kernel_size = [5,5], 
+                                    padding = "SAME", 
+                                    activation_fn = None,
+                                    weights_regularizer=tf.contrib.layers.l1_regularizer(eps_regularization)
+                                    )
+    pool2 = tf.contrib.layers.max_pool2d(inputs = conv2, kernel_size = [2,2], stride = [1,1])
+    pool1_flat = tf.contrib.layers.flatten(pool1, [-1, 7*7*64])
+    pool2_flat = tf.contrib.layers.flatten(pool2, [-1, 7*7*64])
+    concat = tf.concat([pool1_flat, pool2_flat], 1)
+    fc = tf.contrib.layers.fully_connected(inputs = concat, 
+                                           num_outputs = 1024, 
+                                           activation_fn = tf.nn.relu,
+                                           weights_regularizer=tf.contrib.layers.l1_regularizer(eps_regularization)
+                                           )
+    output = tf.contrib.layers.fully_connected(inputs = fc, 
+                                               num_outputs = 2, 
+                                               activation_fn = tf.nn.relu)
+    return output   
 
 #Helper functions.
 def getModelName(datasets):
